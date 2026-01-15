6cff9fe1b2bd8746f11ef01a3e039333101f48c6
Sando8300 <sand2073@gmail.com>
2026-01-15 17:47:20 +0900

    gun
    latsupdate

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public AudioSource Audio;
+    public ParticleSystem gunParticle;
+    public AudioClip[] gunSounds;
+    Dictionary<string, AudioClip> gunDic;
+
+    int shotCount = 0;
+    int overhit = 10;
+    int coolingTime = 3;
+    int overheatTime = 5;
+    bool isOverheat = false;
+    bool cooling = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        // 딕셔너리 초기화
+        gunDic = new Dictionary<string, AudioClip>();
+        for (int i = 0; i < gunSounds.Length; i++)
+        {
+            // 주의: 오디오클립 이름이 "ShotgunSFX", "OverheatSFX"여야 함
+            gunDic.Add(gunSounds[i].name, gunSounds[i]);
+        }
+    }
+    public void ShotLogic()
+
+    {
+
+        if (isOverheat)
+
+        {
+
+            //딸깍소리출력
+
+            return;
+
+        }
+
+        if (shotCount >= overhit && !isOverheat)
+
+        {
+
+            isOverheat = true;
+
+            Audio.PlayOneShot(gunDic["OverheatSFX"]);
+
+            StartCoroutine(Overheat());
+
+            return;
+
+        }
+
+        gunParticle.Play();
+
+        Audio.PlayOneShot(gunDic["ShotgunSFX"]);
+
+        shotCount++;
+        StartCoroutine(Cooldown());
+
+    }
+
+
+
+    IEnumerator Overheat()
+
+    {
+
+        yield return new WaitForSeconds(overheatTime);
+
+        isOverheat = false;
+
+    }
+
+
+
+    IEnumerator Cooldown()
+
+    {
+
+        while (true)
+        {
+            if (shotCount >= 1 && !cooling)
+            {
+                cooling = true;
+
+                yield return new WaitForSeconds(coolingTime);
+
+                shotCount--;
+
+                cooling = false;
+            }
+
+        }
+
+    }
+}
